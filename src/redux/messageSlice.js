7dd0a0e0d46535fabbd380a7f7b8e895c96530a1
JayDungrani <jaydungrani11@gmail.com>
2025-03-24 21:51:24 +0530

    completed await in addmessage

--- src/redux/messageSlice.js
+++ src/redux/messageSlice.js
@@ -26,9 +26,9 @@ const messageSlice = createSlice({
     name : "message",
     initialState : {messageList : [], loading : false},
     reducers : {
-        addMessage : (state, action)=>{
+        addMessage : async(state, action)=>{
             const message = action.payload;
-            axios.put(`${import.meta.env.VITE_API_BASE_URL}/api/message/read/${message.chat}`,{withCredentials : true});
+            await axios.put(`${import.meta.env.VITE_API_BASE_URL}/api/message/read/${message.chat}`,{withCredentials : true});
             state.messageList.push(message);
         }
     },
